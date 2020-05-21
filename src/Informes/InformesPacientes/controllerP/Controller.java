43f293037db147b8b393019b854e647f8ab6f313
negisaro <nesalaz56@gmail.com>
2020-05-21 16:28:21 -0500

    Formularios actualizados

--- src/Informes/InformesPacientes/controllerP/Controller.java
+++ src/Informes/InformesPacientes/controllerP/Controller.java
@@ -1,11 +1,13 @@
 package Informes.InformesPacientes.controllerP;
 
 import conexionBD.ConexionRoot;
+import javafx.scene.control.Alert;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.view.JasperViewer;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 
 public class Controller {
@@ -14,8 +16,12 @@ public class Controller {
         try {
             JasperPrint reportePersonalLeno = JasperFillManager.fillReport(REPORTE_EMPLEADOS,new HashMap<>(), ConexionRoot.getConexion());
             return reportePersonalLeno;
-        } catch (JRException e) {
-            e.printStackTrace();
+        } catch (RuntimeException | JRException ex) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Excepción");
+            alert.setHeaderText("Ocurrio el Error SQL:");
+            alert.setContentText(ex.getLocalizedMessage());
+            alert.show();
         }
         return null;
     }
